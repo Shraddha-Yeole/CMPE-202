8a7833342fa6ca0e08e69951ed68b9dad1271623
Shraddha-Yeole <shraddhayeole15@gmail.com>
2017-03-18 23:14:33 -0700

    Update JavaparserInput.java

--- JavaparserInput.java
+++ JavaparserInput.java
@@ -1,58 +1,15 @@
 package javatouml.parsejava;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-
-
-
 public class JavaparserInput {
-
-	String outputDir;
-	String classpath;
-
-	Map<String, ClassTemplate> classes = new HashMap<String, ClassTemplate>();
-
-	/*
-	public JavaparserInput(String classpath, String outputDir)
-	{
-		this.classpath = classpath;
-		this.outputDir = outputDir;
-	}
-	 */
-
 
 	public static void main(String[] args) throws Exception 
 	{	
-
-		JavaparserInput input = new JavaparserInput(); 
-		input.parseClasses();
-
+		Classesparse c= new Classesparse();
+		c.parseClasses();
+		System.out.println("IntermediateGrammer");
+		String g1=c.getGrammer();
+		System.out.println("PlantUMLGrammer=>\n\n"+g1);
 
 	}	
-
-
-	public void parseClasses() throws Exception {
-		File config = new File("/Users/shraddhayeole/PARSER/parsejava/src/test/java/javatouml/parsejava/testcase");
-		File[] fileset = config.listFiles();
-		if (fileset != null) {
-			for (File javaFile : fileset) {
-				if (javaFile.isDirectory()) {
-					continue;
-				} else if (!(javaFile.getAbsolutePath().endsWith(".java"))) {
-					continue;
-				}
-				ClassTemplate javaClass = Classesparse.getCUnit(new FileInputStream(javaFile));
-				classes.put(javaClass.getClass_Name(), javaClass);
-			}
-
-
-		}
-	}
-
 
 }
