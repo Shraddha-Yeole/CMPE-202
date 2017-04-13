2da316289feea1315910d1f52d774e4dccab36c7
Shraddha-Yeole <shraddhayeole15@gmail.com>
2017-04-13 00:52:41 -0700

    Changed type of return_type from string to type

--- VariableInfo.java
+++ VariableInfo.java
@@ -1,17 +1,19 @@
 package javatouml.parsejava;
 
+import com.github.javaparser.ast.type.Type;
+
 public class VariableInfo {
 
 	private String name;
-	private String data_type;
-	private String init_value;
+	private Type data_type;
+	//private String init_value;
 	private String access_modifier;
 	
-	public VariableInfo(String name, String data_type, String init_value, String access_modifier) {
+	public VariableInfo(String name, Type data_type, String access_modifier) {
 		
 		this.name = name;
 		this.data_type = data_type;
-		this.init_value = init_value;
+		//this.init_value = init_value;
 		this.access_modifier = access_modifier;
 	}
 
@@ -23,21 +25,21 @@ public class VariableInfo {
 		this.name = name;
 	}
 
-	public String getData_type() {
+	public Type getData_type() {
 		return data_type;
 	}
 
-	public void setData_type(String data_type) {
+	public void setData_type(Type data_type) {
 		this.data_type = data_type;
 	}
 
-	public String getInit_value() {
+	/*public String getInit_value() {
 		return init_value;
 	}
 
 	public void setInit_value(String init_value) {
 		this.init_value = init_value;
-	}
+	}*/
 
 	public String getAccess_modifier() {
 		return access_modifier;
@@ -51,7 +53,7 @@ public class VariableInfo {
 	public String toString() {
 		
 		String str =  data_type + " " + name + " :" 
-				+ init_value + " " + access_modifier;
+				+ " " + access_modifier;
 		return str;
 		
 	}
