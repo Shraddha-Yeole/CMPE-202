7dc5de0c3c4d56b7984a17c2e737540f7c8f57a5
Shraddha-Yeole <shraddhayeole15@gmail.com>
2017-04-22 23:15:28 -0700

    Update ClassTemplate

--- ClassTemplate.java
+++ ClassTemplate.java
@@ -1,26 +1,34 @@
-package javatouml.parsejava;
+
 
 import java.util.ArrayList;
 import java.util.Collection;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-//import com.github.javaparser.ast.Modifier;
+import java.util.Set;
 
-//import com.github.javaparser.ast.type.Type;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
 
 public class ClassTemplate {
 
 	private String class_Name;
 	private String extendz;
 	private boolean isInterface = false;
-	
+
 	private Collection<String> interfaces = new ArrayList<String>();
 	private Map<String, MethodClass> methodMap = new HashMap<String, MethodClass>();
 	Map<String, VariableInfo> varmap= new HashMap<String, VariableInfo>();
-	
+	Map<String,Set<String>> refvarmap=new HashMap<String,Set<String>>();
+	static Set <String> refervariable= new HashSet<String>();
+	public List<ClassOrInterfaceType> extendsList = new ArrayList<ClassOrInterfaceType>();
+	public List<ClassOrInterfaceType> implementz = new ArrayList<ClassOrInterfaceType>();
+
+
+
 	public ClassTemplate(String class_Name) {
-		
+
 		this.class_Name = class_Name;
 	}
 
@@ -31,8 +39,6 @@ public class ClassTemplate {
 	public void setClass_Name(String class_Name) {
 		this.class_Name = class_Name;
 	}
-	
-	
 
 	public String getExtendz() {
 		return extendz;
@@ -50,8 +56,6 @@ public class ClassTemplate {
 		this.isInterface = isInterface;
 	}
 
-	
-	
 	public Collection<String> getInterfaces() {
 		return interfaces;
 	}
@@ -60,21 +64,10 @@ public class ClassTemplate {
 		this.interfaces = interfaces;
 	}
 
-	
 	public void addInterface(String name) {
 		interfaces.add(name);
 	}
-	
-	/*
-	public  void setVariables(Map<String, VariableInfo> variables) {
-		variablemap = variables;
-	}
 
-	public  void addVariable(String vName, VariableInfo variable) {
-		variablemap.put(vName, variable);
-	}
-	*/
-	
 	public Map<String, MethodClass> getMethodMap() {
 		return methodMap;
 	}
@@ -82,27 +75,15 @@ public class ClassTemplate {
 	public void setMethodMap(Map<String, MethodClass> methodMap) {
 		this.methodMap = methodMap;
 	}
-	
+
 	public void addMethod(String mName, MethodClass method) {
 		methodMap.put(mName, method);
 	}
 
-	/*
-	public Collection<VariableInfo> getVariables() {
-		
-		return variablemap.values();
+	public Collection<MethodClass> getMethods() {
+		return methodMap.values();
 	}
 
-*/	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
+
 
